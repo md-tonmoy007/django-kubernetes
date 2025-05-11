57e62c07d83e5c91ad150df297510968f5f58470
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2025-05-11 08:20:47 +0600

    buid action: added env variables

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -26,9 +26,11 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 ENV_ALLOWED_HOST = os.environ.get("ENV_ALLOWED_HOST")
+print("ENV_ALLOWED_HOST:", ENV_ALLOWED_HOST)
 ALLOWED_HOSTS = []
 if ENV_ALLOWED_HOST:
     ALLOWED_HOSTS = [ ENV_ALLOWED_HOST ]
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
 
 
 
